11ad38083cfd521907e04165fed7eafabbf9596a
Andreu <andreu.santaren@gmail.com>
2022-07-26 23:50:24 +0200

    Concat experiments, test 1.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,39 @@ fn main() {
 
     // YOUR CODE GOES HERE
     let script = r#"
-        
+        { @@ print } def test_one
+        test_one symbol_1
+
+        print_stack
+        print_ret_stack
+
+        '------' print
+
+        { { @@ } exe symbol_2 print } def test_two
+        test_two
+
+        print_stack
+        print_ret_stack
+
+        '------' print
+
+        { 0 @@ @def } def zero
+        zero my_zero
+        my_zero print
+
+        "Create a clone of def"
+        { @@ @def } def var
+
+        'Andreu' var name
+        name print
+
+        { 'Hello Andreu' print } var hello
+        hello
+
+        "Create a clone of the '@' word"
+        { @@ } def at
+
+        at any_word print
     "#;
 
     // Create pack and register plugins
@@ -23,6 +55,9 @@ fn register(pack: &mut Pack) {
     pack.dictionary.native("print", print);
     pack.dictionary.native("print_stack", print_stack);
     pack.dictionary.native("print_ret_stack", print_ret_stack);
+
+    pack.dictionary.native("@@", atat);
+    pack.dictionary.native("@def", atdef);
 }
 
 fn print(pack: &mut Pack) -> Result<bool, runpack::Error> {
@@ -79,11 +114,57 @@ De manera que la definició de word pugui obtenir coses del concat del lloc on h
 És més, també podríem voler cridar una de les paraules "standard" que fan servir el concat amb una referència que no es troba 
 al just després d'elles:
 
-{ 0 @def } def zero
+{ ... } def zero
 zero my_var
 
-AAquesa paraula hipotètica, @def, pren una cel·la del concat de zero, no del seu propi concat.
+Aquesa paraula hipotètica, zero, pren una cel·la del seu concat i defineix una paraula que conté un 0.
+
+En realitat només ens cal una paraula, @@, que actua com @ però del concat "extern".
+Després podríem tenir versions de totes les altres paraules que fan servir la pila en comptes del concat.
+
+{ 0 @@ @def } def zero
+zero my_var
+
+Aquest @def pren de la pila una ref a una paraula, en comptes del concat. Aquesta ref hi ha estat ficada per @@.
 
 Com ho fem?
 
+Quan executem una paraula, la següent pos del concat es fica a la pila de retorn, per tant n'hi hauria d'haver
+prou de llegir aquest index i obtenir una paraula del concat d'allà.
 */
+
+fn atat(pack: &mut Pack) -> Result<bool, runpack::Error>  {
+    if let Some(parent_concat_pos) = pack.ret.pop() {
+        if let Some(cell) = pack.concat.array.get(parent_concat_pos) {
+            pack.ret.push(parent_concat_pos + 1);
+            pack.stack.push(cell.clone());
+            Ok(true)
+        }
+        else {
+            Err(runpack::Error::new("atat: couldn't get a cell from the concat".into(), 1000))
+        }
+    }
+    else {
+        Err(runpack::Error::new("atat: couldn't get ret pos".into(), 1000))
+    }
+}
+
+// Usage: 10 @ num @def
+fn atdef(pack: &mut Pack) -> Result<bool, runpack::Error> {
+    let (word, data) = (pack.stack.pop(), pack.stack.pop());
+    if let Some(Cell::Word(word)) = word {
+        if let Some(Cell::Block(block)) = data {
+            pack.dictionary.block(&word, block);
+        }
+        else if let Some(cell) = data {
+            pack.dictionary.data(&word, cell);
+        }
+        else {
+            return Err(runpack::Error::new("atdef: Expecting a block or a cell".into(), 1000));
+        }
+    }
+    else {
+        return Err(runpack::Error::new("atdef: Expecting a word in the stack".into(), 1000));
+    }
+    Ok(true)
+}
