f86e38f5afc6b8009bf13c6af75eb02490164013
Andreu <andreu.santaren@gmail.com>
2022-07-26 15:59:22 +0200

    Recursivity experiment 2.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use runpack::{Pack, Cell, self};
+use runpack::{Pack, Cell, DictEntry, self};
 use runpack_obj;
 
 fn main() {
@@ -7,18 +7,33 @@ fn main() {
     // YOUR CODE GOES HERE
     let script = r#"
         "TODO: veure diferents maneres de treballar amb el concat de la paraula cridada"
+        
+        lex 'count.'
+            { dup print } def print
+            { -- } def dec
+            { dup 0 <= } def finished?
+            { drop } def clean
+            { recurs count.print count.dec count.finished? { count.clean end } if } def start
+        lex ''
+        
+        10 count.start
+        'Finished!' print
+
+        '-------' print
 
         lex 'count.'
             { dup print } def print
             { -- } def dec
             { dup 0 <= } def finished?
-            
-            { recurs count.print count.dec count.finished? { end } if } def ten
+            { drop } def clean
+            { count.print count.dec count.finished? { count.clean ret } if again count.start } def start
         lex ''
         
-        count.ten
-
+        10 count.start
         'Finished!' print
+
+        print_stack
+        print_ret_stack
     "#;
 
     // Create pack and register plugins
@@ -34,8 +49,11 @@ fn main() {
 fn register(pack: &mut Pack) {
     pack.dictionary.native("print", print);
     pack.dictionary.native("print_stack", print_stack);
+    pack.dictionary.native("print_ret_stack", print_ret_stack);
     pack.dictionary.native("recurs", recurs);
     pack.dictionary.native("end", end);
+    pack.dictionary.native("again", again);
+    pack.dictionary.native("ret", ret);
 }
 
 fn print(pack: &mut Pack) -> Result<bool, runpack::Error> {
@@ -65,13 +83,26 @@ fn print_stack(pack: &mut Pack) -> Result<bool, runpack::Error>  {
     Ok(true)
 }
 
+fn print_ret_stack(pack: &mut Pack) -> Result<bool, runpack::Error>  {
+    println!("{:?}", pack.ret);
+    Ok(true)
+}
+
 //Experiment: recursive words (tail recursion):
 //And an "end" word to abort recursion.
-//Implementation:
-// { recurs 'Again!' print } def infinite     "Word 'recurs' puts in the ret stack position of itself, and } just returns to it"
+
+//Test 1:
+// { recurs 'Loooooping!' print } def infinite
+//Word 'recurs' puts in the ret stack position of itself, and } just returns to it
+
+/*
+Conclusions: No m'acaba d'agradar.
+
+1. Es comporta com una mena de GOTO.
+2. No veig que estalvii codi ni el faci més clar que emprant un loop.
+ */
 
 fn recurs(pack: &mut Pack) -> Result<bool, runpack::Error> {
-    // push position of current word into ret stack
     pack.ret.push(pack.concat.pointer - 1);
     Ok(true)
 }
@@ -84,5 +115,39 @@ fn end(pack: &mut Pack) -> Result<bool, runpack::Error> {
     }
     else {
         Err(runpack::Error::new("end: Return stack underflow".into(), runpack::ErrCode::StackUnderflow.into()))
+    }
+}
+
+//Test 2:
+// { 'Loooping!' print again infinite } def infinite
+
+/*
+Conclusions: Força millor, però encara no veig que sigui millor que un simple loop.
+ */
+
+fn again(pack: &mut Pack) -> Result<bool, runpack::Error> {
+    if let Some(Cell::Word(w)) = pack.concat.next() {
+        if let Some(DictEntry::Defined(block)) = pack.dictionary.dict.get(w) {
+            pack.concat.pointer = block.pos;
+            Ok(true)
+        }
+        else {
+            Err(runpack::Error::new("again: No word".into(), runpack::ErrCode::NotFound.into()))
+        }
+    }
+    else {
+        Err(runpack::Error::new("again: No word in the concat".into(), runpack::ErrCode::NoArgsConcat.into()))
+    }
+}
+
+fn ret(pack: &mut Pack) -> Result<bool, runpack::Error> {
+    // Fa igual que "}"
+    // Discard 1 ret positions, the call to 'ret'
+    if let (Some(_), Some(pos)) = ( pack.ret.pop(), pack.ret.pop()) {
+        pack.concat.pointer = pos;
+        Ok(true)
+    }
+    else {
+        Err(runpack::Error::new("ret: Return stack underflow".into(), runpack::ErrCode::StackUnderflow.into()))
     }
 }
